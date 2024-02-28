cb66c47a2551a7a11626946c655dc4d4da8180e6
Romoluwa <romzy2008@gmail.com>
2024-02-28 21:59:24 +0100

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //we create a date object and set the time to the day we want it to count down to
 //getTime is to get the timestamp in milliseconds
-var countDownDate = new Date ("Mar 2, 2024").getTime();
+var countDownDate = new Date ("March 3, 2024").getTime();
 //we set an interval that is executed every one second(1000millisecond)
 var x = setInterval(function(){
 //this collects the current date and time and assigns it to the  variable now
@@ -13,15 +13,24 @@ var hours = Math.floor((distance %(1000 * 60 *60 * 24)) / (1000 *60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-//below we want to write a code that when time id is not written in double digits  a zero can ber added to it from behind
-days = days < 10 ? "0" + days : days
-hours =hours < 10 ? "0" + hours : hours
-minutes =minutes < 10 ? "0" + minutes : minutes
-seconds =seconds < 10 ? "0" + seconds  : seconds
+ //below we want to write a code that when time id is not written in double digits  a zero can ber added to it from behind
+ days = days < 10 ? "0" + days : days
+ hours =hours < 10 ? "0" + hours : hours
+ minutes =minutes < 10 ? "0" + minutes : minutes
+ seconds =seconds < 10 ? "0" + seconds  : seconds
 
 //this with display the upadate time in the html file
 document.getElementById("days").innerHTML = days;
 document.getElementById("hours").innerHTML = hours;
 document.getElementById("minutes").innerHTML = minutes;
 document.getElementById("secs").innerHTML = seconds;
+
+if (distance < 0){
+    setInterval(x);
+    countDownDate = new Date(now + 10 * 24 * 60 * 60 * 1000).getTime(); //reset days to be 10 days ahead
+    document.getElementById("days").innerHTML = "10"; //set days to 10
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("secs").innerHTML = "00";
+}
 },1000);
